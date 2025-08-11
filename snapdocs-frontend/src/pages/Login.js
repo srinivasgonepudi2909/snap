5d52c9fff0d8beb2803b56bab5b7eebfe996a6ca
srinivas <srigonepudi@gmail.com>
2025-08-11 15:10:45 +0530

    updated

--- snapdocs-frontend/src/pages/Login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginComponent from '../components/auth/Login';
-
-const Login = () => {
-  return <LoginComponent />;
-};
-
-export default Login;
--- snapdocs-frontend/src/pages/Signup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SignupComponent from '../components/auth/Signup';
-
-const Signup = () => {
-  return <SignupComponent />;
-};
-
-export default Signup;
