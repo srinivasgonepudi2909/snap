6e01ccf9e8540846729d69fef2f75fa53d79edff
srinivas <srigonepudi@gmail.com>
2025-08-22 11:06:33 +0530

    updated

--- backend/services/auth-service/app/utils/config.py
+++ backend/services/auth-service/app/utils/config.py
@@ -1,20 +1,24 @@
-import os
-from dotenv import load_dotenv
+# app/utils/config.py
+
 from pymongo import MongoClient
-# app/config.py
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    SECRET_KEY: str = "supersecretkey"  # Use env var in production
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+    MONGO_URI: str
+
+    class Config:
+        env_file = ".env"  # Automatically loads this file
+
+
+# Create settings instance
 settings = Settings()
 
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-
+# MongoDB connection using the URI from settings
+client = MongoClient(settings.MONGO_URI)
 db = client["snapdocs-auth"]
 user_collection = db["users"]
