22714e993019e28d60dd9273f507370ab931524f
srinivas <srigonepudi@gmail.com>
2025-08-23 13:10:50 +0530

    updated

--- database/init.js
+++ database/init.js
@@ -1,10 +1,25 @@
-db = db.getSiblingDB('snapdocs');
+// Switch to the "admin" database for user creation
+db = db.getSiblingDB("admin");
 
-db.createCollection('users');
+db.createUser({
+  user: "Chaitra",
+  pwd: "changeme123",
+  roles: [
+    {
+      role: "readWrite",
+      db: "snapdocs"
+    }
+  ]
+});
+
+// Now switch to the target database and create sample data
+db = db.getSiblingDB("snapdocs");
+
+db.createCollection("users");
 
 db.users.insertOne({
   name: "Chaitra",
   email: "chaitra@example.com",
-  password: "changeme123", // Use hashed password in production
+  password: "changeme123",
   role: "admin"
 });
