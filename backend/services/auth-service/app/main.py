9756e24413c932f185325f98cdea8d6f82873984
srinivas <srigonepudi@gmail.com>
2025-08-27 13:20:45 +0530

    updated

--- backend/services/auth-service/app/main.py
+++ backend/services/auth-service/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://54.157.33.226:3000",  # Add your EC2 public IP
+    "http://54.160.240.204:3000",  # Add your EC2 public IP
     os.getenv("FRONTEND_URL"),
     "*"  # Allow all origins for development (remove in production)
 ]
